fbe08e613e31480ddff87bda44331e02443d9cb2
Fethi BEN ABDELHAMID <fethi.env@gmail.com>
2020-08-29 12:47:01 +0100

    fix components

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,13 +11,7 @@
 </template>
 
 <script>
-import Header from '~/components/global/Header.vue'
-import Footer from '~/components/global/Footer.vue'
-
 export default {
-  components: {
-    Header,
-    Footer,
-  },
+  components: {},
 }
 </script>
